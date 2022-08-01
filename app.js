5202ffc79a693614eacec02db5e4af1c22178e7f
rijalamisha21 <rijalamisha17@gmail.com>
2022-08-01 10:49:00 +0545

    width manage by js

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const profile= document.getElementById("profile")
 const navbar= document.getElementsByClassName("nav__bar")
+const article= document.getElementsByClassName("middle-article")
 const collapse= document.getElementById("collapse");
 const setting= document.getElementsByClassName("wrap__setting");
+
 
 //use toggle 
 collapse.addEventListener("click", ()=>{
     profile.style.display= "none";
-    navbar[0].style.width= "9rem";
+    navbar[0].style.width= "6%";
+    article[0].style.width= "94%"
+
+
     const text = document.getElementsByClassName("setting__text");
     for(i=0; i<text.length; i++){
         text[i].style.display= "none";
